e087e652cfb29e01d96cd1237227d5395146de00
shgtkshruch <shgtk.shruch@gmail.com>
2016-06-24 11:56:18 +0900

    Initial commit

---
+++ Assets/Scripts/DangerWall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour {
+	void OnCollisionEnter (Collision hit) {
+		if (hit.gameObject.CompareTag ("Player")) {
+			// 現在のシーン番号を取得
+			int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+			// 現在のシーンを再読み込みする
+			SceneManager.LoadScene(sceneIndex);
+		}
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
+	void Update () {
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabel.text = count.ToString();
+
+		// すべてのアイテムが消えたら、クリアメッセージを出す
+		if (count == 0) {
+			winnerLabelObject.SetActive (true);
+		}
+	}
+}
